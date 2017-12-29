eec69908a00e6cd55861a66d15317fd96ac0b6a9
vardking <vardking@gmail.com>
2017-12-29 23:29:29 +0800

    simple adjust pure

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+const del        = require('del');
+const gulp       = require('gulp');
+const sass       = require('gulp-sass');
+const concat     = require('gulp-concat');
+const postcss    = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+const autoprefixer = require('autoprefixer');
+
+gulp.task('default', ['clean', 'sass'/*, 'webpack'*/], function () {
+    gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('clean', function () {
+    return del(['./dist/*']);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/**/*.scss')
+    .pipe(sourcemaps.init())
+    //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expand'}).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# ui-toolkit
+front-end toolkit based on pure, bootstrap, etc.
