c2837244b0a27e57d3d36904f0b965c2bd0423c5
vardking <vardking@gmail.com>
2017-12-30 23:46:05 +0800

    extract carousel from bootstrap

--- dist/js/modal/modal.js
+++ dist/js/modal/modal.js
@@ -1,8 +1,11 @@
+'use strict';
+
 /**
  * Created by vd on 30/12/17.
  */
 ;$(function () {
     'use strict';
+
     $('.modal-close').click(function () {
         $('.modal').removeClass('is-active');
     });
