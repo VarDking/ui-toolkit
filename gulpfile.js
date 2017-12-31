c3cc12cc59ceb1d9061154cbbaa8a3756dad195d
vardking <vardking@gmail.com>
2017-12-31 17:02:56 +0800

    update gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 'use strict';
-const del        = require('del');
-const gulp       = require('gulp');
-const sass       = require('gulp-sass');
-const concat     = require('gulp-concat');
-const postcss    = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
-const babel      = require('gulp-babel');
+const del          = require('del');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const concat       = require('gulp-concat');
+const postcss      = require('gulp-postcss');
+const sourcemaps   = require('gulp-sourcemaps');
+const babel        = require('gulp-babel');
+const gulpSequence = require('gulp-sequence');
 
 const autoprefixer = require('autoprefixer');
 
-gulp.task('default', ['clean', 'sass', 'compile-js'], function () {
-    gulp.watch('./src/**/*.scss', ['sass']);
-    gulp.watch('./src/**/*.js', ['compile-js']);
+gulp.task('default', function () {
+    gulpSequence(['clean'], ['sass', 'compile-js'], function () {
+        gulp.watch('./src/**/*.scss', ['sass']);
+        gulp.watch('./src/**/*.js', ['compile-js']);
+    });
 });
 
 gulp.task('clean', function () {
@@ -21,14 +24,12 @@ gulp.task('clean', function () {
 gulp.task('sass', function () {
     return gulp.src('./src/all.scss')
     .pipe(sourcemaps.init())
-    //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(sass({outputStyle: 'expand'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
     .pipe(concat('all.css'))
     .pipe(gulp.dest('./dist/css'));
 });
-
 
 gulp.task('compile-js', function () {
     return gulp.src('./src/**/*.js')
