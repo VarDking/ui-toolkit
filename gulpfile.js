ec81966bb873c95159d1bd3df6d44efa9a5f72e1
vardking <vardking@gmail.com>
2018-01-01 01:20:18 +0800

    remove sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const gulp         = require('gulp');
 const sass         = require('gulp-sass');
 const concat       = require('gulp-concat');
 const postcss      = require('gulp-postcss');
-const sourcemaps   = require('gulp-sourcemaps');
 const babel        = require('gulp-babel');
 const gulpSequence = require('gulp-sequence');
 const uglify       = require('gulp-uglify');
@@ -24,10 +23,8 @@ gulp.task('clean', function () {
 
 gulp.task('sass', function () {
     return gulp.src('./src/all.scss')
-    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'));
 });
 
