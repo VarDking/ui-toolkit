7651d814e6d317222966e884a7fdf5e913602da1
vardking <vardking@gmail.com>
2017-12-31 17:30:23 +0800

    uglify and concat js file

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,14 @@ const postcss      = require('gulp-postcss');
 const sourcemaps   = require('gulp-sourcemaps');
 const babel        = require('gulp-babel');
 const gulpSequence = require('gulp-sequence');
+const uglify       = require('gulp-uglify');
 
 const autoprefixer = require('autoprefixer');
 
 gulp.task('default', function () {
-    gulpSequence(['clean'], ['sass', 'compile-js'], function () {
+    gulpSequence(['clean'], ['sass', 'compile-js'], ['concat-js'], function () {
         gulp.watch('./src/**/*.scss', ['sass']);
-        gulp.watch('./src/**/*.js', ['compile-js']);
+        gulp.watch('./src/**/*.js', gulpSequence(['compile-js'], ['concat-js']));
     });
 });
 
@@ -26,9 +27,8 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(concat('all.css'))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('compile-js', function () {
@@ -37,4 +37,13 @@ gulp.task('compile-js', function () {
         presets: ['env']
     }))
     .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('concat-js', function () {
+    return gulp.src('./dist/js/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify({
+        mangle: true
+    }))
+    .pipe(gulp.dest('./dist'))
 });
